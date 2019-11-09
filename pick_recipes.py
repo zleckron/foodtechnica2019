79aa8f2107195d66703fa49e664f9b299aa2abed
zleckron <ki94779@umbc.edu>
2019-11-09 14:25:51 -0500

    match_recipes functionally works and outputs recipes the user can afford. TODO: consideration of quantity

--- pick_recipes.py
+++ pick_recipes.py
@@ -32,15 +32,49 @@ def load_available_ingredients(path):
 """
 def get_user_foods():
     pantry = []
-    food_item = input("Enter a food, \"QUIT\" to exit")
+    food_item = input("Enter a food, \"QUIT\" to exit: ")
     while food_item != QUIT:
         pantry.append(food_item)
+        food_item = input("Enter a food, \"QUIT\" to exit: " )
     return pantry
+
+"""
+    CN: Reconsier recipe structure -- should hold quantity of each item
+    Can write a file for content to cost conversion
+    assuming recipes is a list of dictionaries (we will worry later about storing the instructions)
+    the dictionary holds "ingredient":price pairs
+    will return a 3D list holding [recipe, []] <-- with the second element as a list of to_buy goods
+"""
+def match_recipes(pantry, recipes, max_cost):
+    matches = []
+    #for every recipe, get the price difference we would need to complete it
+    #CN: ASSUMING 1 POUND/DOZEN/GALLON OF EVERYTHING NEEDED
+    for recipe in recipes:
+        cost = 0.0
+        to_buy = []
+        needed_goods = recipe.keys()
+        #for each ingredient required, we only care if we don't already have it
+        #TODO: let user indicate quantity of ingredients that they have
+        for good in needed_goods:
+            if not (good in pantry):
+                to_buy.append(good)
+                #will give us the cost of the food in this recipe
+                cost += recipe[good]
+        if cost < max_cost:
+            #add this recipe to matches
+            matches.append([recipe, to_buy])
+    return matches
 
 if __name__ == "__main__":
     #load all recipe files in RECIPE_DIR into some list
-    available = load_available_ingredients("Basic_Ing.csv")
-    print(available)
-    pantry = get_user_foods
+    #available = load_available_ingredients("Basic_Ing.csv")
+    #print(available)
+    user_pantry = get_user_foods()
 
     #A test: what recipies can we make given this food
+    #hardcoding to test stuff out
+    recipe_list = [{"eggs": 1.0, "milk": 4.0, "butter": 17.0}]
+
+    matching = match_recipes(user_pantry, recipe_list, MAX_PRICE_HARDCODE)
+
+    print(matching)
