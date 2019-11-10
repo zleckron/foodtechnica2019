b9db68b1822c94be1ba41f1ef9cf0e0a2794c7c0
unknown <jschult1@umbc.edu>
2019-11-10 00:32:19 -0500

    this baby works kind of

--- food.py
+++ food.py
@@ -6,23 +6,25 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def home():
 
-        return render_template('index.html')
+    ingredients = ["White Flour","White Rice","Pasta","Bread","Chocolate Chip Cookies","Uncooked Ground Beef",
+        "Uncooked Beef Roasts","Uncooked Beef Steaks","Bacon","Pork Chops","Ham","Bologna","Chicken Breast","Chicken Leg",
+        "Turkey","Eggs","Milk","Cheddar Cheese","Ice Cream","Bananas","Oranges","Grapes","Lemons","Peaches","White Potatoes",
+        "Iceberg Lettuce","Tomatoes","Broccoli","Sweet Peppers","Orange Juice","Beans","White Sugar","Margarine"]
+
+    return render_template('index.html', ingredients_list=ingredients)
 
 @app.route("/recipe", methods=["get", "post"])
 def recipe():
 
     if request.method == 'POST':
-        #return request.form
-        ing1 = request.form["ing1"]
-        ing2 = request.form["ing2"]
-        ing3 = request.form["ing3"]
 
-        return "Ingredients: " + str(ing1) + ing2 + ing3
+        #return "Ingredients: " + ing1 + ing2 + ing3
+
+        return request.form
 
     else:
         print(request.method)
-        return ("fuck me")
-
+        return ("oh no")
 
 if __name__ == "__main__":
     app.run()
