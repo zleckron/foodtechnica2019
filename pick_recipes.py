50c020031b1379b44d516eba2206f765579475f7
zleckron <ki94779@umbc.edu>
2019-11-09 21:17:13 -0500

    Updated file to test on a recipe csv (with values as the prices of foods required in the recipes)

--- pick_recipes.py
+++ pick_recipes.py
@@ -57,15 +57,23 @@ def match_recipes(pantry, recipes, max_cost):
         #for each ingredient required, we only care if we don't already have it
         #TODO: let user indicate quantity of ingredients that they have
         for good in needed_goods:
-            if not (good in pantry):
-                to_buy.append(good)
-                #will give us the cost of the food in this recipe
-                cost += recipe[good]
+            #print(recipe[good])
+            if good != "Title":
+                if not (good.lower() in lower_list(pantry)):
+                    to_buy.append(good)
+                    #will give us the cost of the food in this recipe
+                    cost += recipe[good]
         if cost < max_cost:
             #add this recipe to matches
             matches.append([recipe, to_buy])
     return matches
 
+#convert raw_list of strings into a list with all lower case values (return new list)
+def lower_list(raw_list):
+    lower = []
+    for elem in raw_list:
+        lower.append(elem.lower())
+    return lower
 """
     :param recipes: a list of recipes, where is recipe is a dictionary of food:quanity pairs
     NOTE: eggs, milk, and ice cream have special units!!!
@@ -88,16 +96,39 @@ def special_unit_price(available, item, amount):
         return
     return 420.0
 """
+
+def make_recipies(path):
+    recipes = []
+    recipefp = open(path, "r")
+    recipes_lines = recipefp.readlines()
+    recipefp.close()
+    #for each line, first item is the title
+    for raw_line in recipes_lines:
+        line = raw_line.strip()
+        new_recipe = {}
+        line_data = line.split(COMMA)
+        #print(line_data)
+        #remember that the first
+        new_recipe["Title"] = line_data[0]
+        for i in range(1, len(line_data) - 2, 2):
+            new_recipe[line_data[i]] = float(line_data[i+1])
+        recipes.append(new_recipe)
+    return recipes
+
 if __name__ == "__main__":
     #load all recipe files in RECIPE_DIR into some list
     #available = load_available_ingredients("Basic_Ing.csv")
     #print(available)
-    user_pantry = get_user_foods()
 
     #A test: what recipies can we make given this food
     #hardcoding to test stuff out
+    my_recps = make_recipies("Recipes_Prices.csv")
+    #print(my_recps)
     recipe_list = [{"bread": 1.0, "oil": 4.0, "butter": 17.0}]
+    user_pantry = get_user_foods()
+    budget = float(input("What's your budget? "))
 
-    matching = match_recipes(user_pantry, recipe_list, MAX_PRICE_HARDCODE)
+    matching = match_recipes(user_pantry, my_recps, budget)
 
-    print(matching)
+    print("YOUR RECIPES ma'am:", matching)
+    #print(my_recps)
